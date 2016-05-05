f32fffd554c40d030f0fe3f59f514ca5efa1272d
Timur <injener89@gmail.com>
2016-05-05 22:32:00 +0500

    bot_api.php

--- bot_api.php
+++ bot_api.php
@@ -1,4 +1,10 @@
 <?php
+$API_KEY = '187767986:AAFLK_h8SMMG1mH8cDS64xfE5Lb6LRyo2vU';
+$url = 'https://api.telegram.org/bot'.$API_KEY;
+$update = file_get_contents($url."/getupdates");
+$updateArray = json_decode($update,TRUE);
 
-echo '{"ok":false,"error_code":404,"description":"[Error : 404 : Method not found]"}';
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+$update = file_get_contents($url."/sendmessage?chat_id=".$chatId."&text=Maxalchik");
 ?>
